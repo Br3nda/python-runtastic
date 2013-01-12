da7e4143713af9b0fc26f4ea2df389b630e31367
Brenda Wallace <shiny@cpan.org>
2013-01-12 16:20:31 +1300

    start of runtastic api

---
+++ runtastic/__init__.py
@@ -0,0 +1,59 @@
+import sqlite3
+import urllib
+import logging
+import json
+import urllib2
+import re
+
+logger = logging.getLogger('runtastic')
+_URL = 'http://www.runtastic.com/api/'
+
+def request(path, postfields):
+    headers = {}
+
+    params = urllib.urlencode(postfields)
+    url = _URL + path
+
+    req = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(req, params)
+
+    #parse from json
+    return json.loads(response.read())
+
+class TrainingHistory(object):
+    
+    def __init__(self, user_id):
+        self.user_id = user_id
+        
+    def month(self, month):
+        activities = []
+        for data in request('statistics/training_history/month',  postfields={'compare': None,	
+            'field':	'count',
+            'js_timezone_offset':	-780,
+            'kind':	1,
+            'locale':	'en',
+            'period':	'month',
+            'period_end':	month,
+            'sport_types':	3,
+            'user_id':	self.user_id,
+            'users': None}):
+            
+            a = Activity(activity_id=data[0])
+            a.populate(data)
+            activities.append(a)
+        return activities
+    
+class Activity(object):
+    def __init__(self, activity_id):
+        self.activity_id = activity_id
+        
+    def populate(self, data):
+        """
+        Populate with data from runtastic query
+        """
+        self.data = data
+    def __str__(self):
+        return "{id} {data}".format(id=self.activity_id, data=self.data)
+        
+
+
